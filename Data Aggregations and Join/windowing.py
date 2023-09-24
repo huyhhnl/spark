24ccbe141e50d53b74e503e7f98d5852866e3f96
huyhhnl <110181079+huyhhnl@users.noreply.github.com>
2023-09-24 22:38:06 +0700

    Create windowing.py

---
+++ Data Aggregations and Join/windowing.py
@@ -0,0 +1,19 @@
+from pyspark.sql import Window
+conf = SparkConf() \
+    .setMaster('local') \
+    .setAppName('WindowingDemo')
+
+spark = SparkSession.builder.config(conf=conf).getOrCreate()
+sc = spark.sparkContext
+
+DATASET_PATH = 'drive/MyDrive/.../summary.parquet'
+
+summary_df = spark.read.parquet(DATASET_PATH)
+summary_df.printSchema()
+
+running_total_window = Window.partitionBy('Country')\  # tạo cửa sổ trượt
+                             .orderBy('WeekNumber')\
+                             .rowsBetween(Window.unboundedPreceding, Window.currentRow)
+
+summary_df.withColumn("RunningTotal",sum('InvoiceValue').over(running_total_window)) \  # tính tổng theo cửa sổ - tổng tích lũy
+    .show()
